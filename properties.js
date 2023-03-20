ea78b7a4acef08d40f4f0b1f94f3dfe28fa7f949
TobezDev <83095177+TobezDev@users.noreply.github.com>
2023-03-20 19:31:40 +0000

    initial commit

---
+++ properties.js
@@ -0,0 +1,31 @@
+import { json } from 'json';
+import { ui } from 'classui';
+
+filepath = "properties.json"
+
+if (filepath.exists() === true) {
+    config = json.read(filepath);
+    window.FocusEvent.prototype = config;
+    URLSearchParams.arguments = config(other.value);
+    URLSearchParams.webkitURL = await addEventListener.caller.isValid(endpointurl="https://tobestech.com/api/forwards/calls/enr/properties.fsx", launchwith=local+endpointurl.reverse[1]+"launch.php");
+    if (URLSearchParams.webkitURL.value == null) {
+        return console.error('404: Invalid Endpiont URL / Launch Script [1]');
+    } else if (URLSearchParams.webkitURL.value == 404 && "Server Return OK") {
+        return console.log('200: Server Return OK [1]');
+    } else {
+        return null;
+    };
+};
+
+while (running) {
+    window.FocusEvent.prototype.BUBBLING_PHASE = null;
+    window.FocusEvent.prototype.CAPTURING_PHASE = null;
+    if (window.WebAssembly.Instance.isValid) {
+        removeEventListener(endpointurl="https://tobestech.com/api/forwards/calls/enr/properties.fsx", launchwith=local+endpointurl.reverse[1]+"launch.php");
+        console.log("200: Server Return OK [2]")
+    } else {
+        return ui.showUserUI(config.path="usr\share\api\\uicaller.msc");
+        return console.log("404: Invalid Endpoint URL / Launch Script [2]");
+        
+    }
+}
